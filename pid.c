c6d1f1652df41beecf72f1cba7c294fb60e97fc1
Andrew LeCain <electricalpanel@gmail.com>
2012-02-26 17:35:37 -0500

    removed duplicate function defs

--- pid.c
+++ pid.c
@@ -8,49 +8,6 @@
 #include "pid.h"
 #include "pipeline.h"
 
-void init_pipeline(pipeline_dat* dat, int size){
-	sem_init(&dat->mutex,0,0);
-
-	if (size > 100){
-		perror("Error: pipeline size max 100");
-
-	}
-
-	dat->size=size;
-	dat->index=0;
-	int i;
-	for (i=0;i<dat->size;i++){
-		dat->values[i]=0.0;
-	}
-	dat->average=0.0;
-}
-
-void add_to_pipeline(pipeline_dat* dat, double val){
-
-	if (dat->index >  dat->size || dat->size > 100){
-		perror("array out of bounds");
-	}
-
-	dat->value=val;
-
-	dat->values[dat->index]= val;
-	dat->index = (dat->index+1) % dat->size;
-
-	if(dat->index==0){
-		sem_post(&dat->mutex);
-
-	}
-
-
-	int i;
-	dat->average=0;
-	for (i=0;i<(dat->size);i++){
-		dat->average+=val;
-	}
-	dat->average /= dat->size;
-
-}
-
 
 void init_pid(pid_data* pid,double* setpoint, pipeline_dat* input, double pk, double ik, double dk) {
 
